bf37b74633eff2458a8f26c987c6eb8862ae8d35
vaibhav tyagi <vaibhav31122@gmail.com>
2020-06-22 23:35:01 +0530

    features added

--- src/components/NavBar/ToolBar/Toolbar.js
+++ src/components/NavBar/ToolBar/Toolbar.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
-import './Toolbar.css';
+import logo from "../../../assets/images/logo.png";
+import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
+import "./Toolbar.css";
+import { Link } from "react-router-dom";
 
-const toolbar = props => (
+const toolbar = (props) => (
   <header className="toolbar">
     <nav className="toolbar__navigation">
-        <div className="toolbar__toggle-button">
-            <DrawerToggleButton click={props.drawerClickHandler} />
-        </div>
-        <div className="toolbar__logo"><a href="/">THE LOGO</a></div>
-        <div className="spacer" />
-        <div className="toolbar_navigation-items">
-            <ul>
-                <li><a href="/">Products</a></li>
-                <li><a href="/">Users</a></li>
-            </ul>
-        </div>
+      <div className="toolbar__toggle-button">
+        <DrawerToggleButton click={props.drawerClickHandler} />
+      </div>
+      <div className="toolbar__logo">
+        <Link to="/">
+          <img src={logo} alt="logo" height="100" width="200" />
+        </Link>
+      </div>
+      <div className="spacer" />
+      <div className="toolbar_navigation-items">
+        <ul>
+          <li>
+            <a href="/about-us">About Us</a>
+          </li>
+          <li>
+            <a href="/contact-us">Contact Us</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   </header>
 );
